d8fd4601e086185e77b7cc997386afc7b9c3bec9
Jonny Moore <jonnymoore121212@gmail.com>
2016-07-12 12:13:44 +0100

    Adds top_up balance test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,9 @@ describe Oystercard do
   it 'shows card balance' do
     expect(subject.balance).to eq 0
   end
+
+  it 'tops up balance' do
+    subject.top_up(10)
+    expect(subject.balance).to eq 10
+  end
 end
