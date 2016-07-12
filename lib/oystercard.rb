e323386fbb43b1f24a9664aa43a00379ced45bd4
Harry Jaggs <harryjaggs92@gmail.com>
2016-07-12 16:53:47 +0000

    Step 8 touch in touch out methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,13 +5,27 @@ attr_reader :balance
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
    fail "Maximum balance of #{MAXIMUM_BALANCE} reached" if balance + amount > MAXIMUM_BALANCE
    @balance += amount
   end
+  
   def deduct(amount)
     @balance -= amount
+  end
+  
+  def touch_in
+    @in_journey = true
+  end
+  
+  def touch_out
+     @in_journey = false
+  end
+  
+  def in_journey?
+     @in_journey
   end
 end
