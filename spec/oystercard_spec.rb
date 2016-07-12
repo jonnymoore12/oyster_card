48cdbf7e4fccfb01c1ee9b29eb75a62e60f97105
6eff <6eff@mail.ru>
2016-07-12 14:34:52 +0300

    tops_up the balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,9 +4,9 @@ describe Oystercard do
   it 'shows card balance' do
     expect(subject.balance).to eq 0
   end
-
-  it 'tops up balance' do
-    subject.top_up(10)
-    expect(subject.balance).to eq 10
+  describe "#top_up" do
+    it 'tops up balance' do
+      expect{subject.top_up(10)}.to change {subject.balance}.by 10
+    end
   end
 end
