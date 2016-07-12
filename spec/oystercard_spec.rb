fbf4e3d845ee7fa03a3af3ff5efd8872c30d5d37
Jonny Moore <jonnymoore121212@gmail.com>
2016-07-12 14:48:31 +0100

    Tests deduct method dedeucts from balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
+
   it 'shows card balance' do
     expect(subject.balance).to eq 0
   end
+
   describe "#top_up" do
     it 'tops up balance' do
       expect{subject.top_up(10)}.to change {subject.balance}.by 10
@@ -14,6 +16,14 @@ describe Oystercard do
       subject.top_up(limit)
       expect{subject.top_up(1)}.to raise_error("limit #{limit} reached")
     end
+  end
+
+  describe "#deduct" do
+    it 'deducts the fare amount from balance' do
+      subject.top_up(100)
+      expect{subject.deduct(50)}.to change {subject.balance}.by -50
+    end
 
   end
+
 end
