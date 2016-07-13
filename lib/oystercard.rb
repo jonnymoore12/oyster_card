fd30ba2dd0a8bbb5a72fcb50f443f061a99fb1d2
Harry Jaggs <harryjaggs92@gmail.com>
2016-07-13 11:14:55 +0100

    step 9 checking minimum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,7 @@
 class Oystercard
 
 MAXIMUM_BALANCE = 90
+MINIMUM_CREDIT = 1
 attr_reader :balance
 
   def initialize
@@ -12,20 +13,26 @@ attr_reader :balance
    fail "Maximum balance of #{MAXIMUM_BALANCE} reached" if balance + amount > MAXIMUM_BALANCE
    @balance += amount
   end
-  
+
   def deduct(amount)
     @balance -= amount
   end
-  
+
   def touch_in
+    raise "No credit on card" if no_credit
     @in_journey = true
   end
-  
+
+
   def touch_out
      @in_journey = false
   end
-  
+
   def in_journey?
      @in_journey
+  end
+
+  def no_credit
+    @balance < MINIMUM_CREDIT
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,8 @@
 require 'oystercard'
 
+TOP_UP_AMOUNT = 5
+DEDUCT_AMOUNT = 5
+
 describe Oystercard do
  it 'balance should equal 0' do
      expect(subject.balance).to eq 0
@@ -19,26 +22,29 @@ describe Oystercard do
 
   describe '#deduct' do
     it 'reduces the balance by specified amount' do
-      subject.top_up 15
-      expect{ subject.deduct 5}.to change{ subject.balance }.by -5
+      subject.top_up TOP_UP_AMOUNT
+      expect{ subject.deduct DEDUCT_AMOUNT}.to change{ subject.balance }.by -DEDUCT_AMOUNT
     end
   end
-  
+
   it 'is initially not in a journey' do
    expect(subject).not_to be_in_journey
   end
-  
+
   it 'can touch in' do
+   subject.top_up TOP_UP_AMOUNT
    subject.touch_in
    expect(subject).to be_in_journey
   end
-  
+
   it 'can touch out' do
+    subject.top_up TOP_UP_AMOUNT
     subject.touch_in
     subject.touch_out
     expect(subject).not_to be_in_journey
   end
-   
+
+  it 'enforces a minimum balance' do
+    expect{subject.touch_in}.to raise_error "No credit on card"
+  end
 end
-
-
